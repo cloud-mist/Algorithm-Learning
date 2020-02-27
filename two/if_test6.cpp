51e90cec7d438cb7dd2f18cae2b63f63de5aec63
zhangxiao <1131350950@qq.com>
2020-02-27 16:51:01 +0800

    2.3

---
+++ two/if_test6.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+int main(){
+	int n = 3, m = 5;
+	if(n < 5){
+		if(m <5){
+			printf("%d\n", m + n);
+		}else{
+			printf("%d\n", m - n);
+		}	
+	}else{
+		printf("haha\n");
+	}
+	return 0;
+}
+
+//if 嵌套循环
